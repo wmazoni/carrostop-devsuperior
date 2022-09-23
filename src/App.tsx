38289b950ed139c3d5cb8f02d3d091a7dfeaa2b8
Wellington Mazoni de Andrade <wmazoni@protonmail.com>
2022-09-23 18:19:00 -0300

    finished home layout xs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import Navbar from './components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <h1> Hello Carros Top</h1>
+      <Home />
     </>
   )
 };
